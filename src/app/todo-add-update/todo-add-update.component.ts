5de662f7c5122ef63705638bb4ea32c546aa791a
Testbook <testbook@Testbooks-MacBook-Pro-3.local>
2022-04-19 10:47:58 +0530

    added close modal

--- src/app/todo-add-update/todo-add-update.component.ts
+++ src/app/todo-add-update/todo-add-update.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OpenModalService } from '../services/open-modal.service';
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-todo-add-update',
