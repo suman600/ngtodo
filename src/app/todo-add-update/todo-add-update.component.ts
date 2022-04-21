7f3922821296a7103d63441117a68309dd934d26
Testbook <testbook@Testbooks-MacBook-Pro-3.local>
2022-04-21 12:18:19 +0530

    added new todo fun

--- src/app/todo-add-update/todo-add-update.component.ts
+++ src/app/todo-add-update/todo-add-update.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { OpenModalService } from '../services/open-modal.service';
-import { FormControl } from '@angular/forms'
+import { FormBuilder, Validators } from '@angular/forms'
+import { Todo } from '../heros/todo';
+import { TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-todo-add-update',
@@ -10,12 +12,50 @@ import { FormControl } from '@angular/forms'
 export class TodoAddUpdateComponent implements OnInit {
   openTodoModal: boolean = false;
 
-  constructor(private openModalService: OpenModalService) {}
+  _todos: Todo[] = [];
+  _todoVal: String = '';
+  _id: Number = 0;
+
+
+  constructor(
+    private openModalService: OpenModalService,
+    private fb: FormBuilder,
+    private todoService: TodoService
+  ) { }
+
+  form = this.fb.group({
+    todoText: ['', Validators.required]
+  })
 
   ngOnInit(): void {
     this.openModalService.openTodoModalEvent.subscribe((event: boolean) => {
       this.openTodoModal = event;
-    });
+    }); 
+    this.getTodos();
+  }
+
+  getTodos() {
+    this.todoService.getTodo()
+    .subscribe((data: Todo) => this._todos = { ...data });
+  }
+
+  
+  
+
+
+  onSubmit() {
+    if (this.form.valid) {
+      this._todoVal = this.form.value.todoText;
+      this._id = this._todos.length + 1;
+      this._todos.push({
+        id: this._id,
+        text: this._todoVal,
+        completed: false,
+        deleted: false
+      })
+    console.log(this._todos)
+    }
+    this.form.reset();
   }
 
   closeTodoModal() {
